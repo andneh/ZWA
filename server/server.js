892d34aaf7c0c3d63896dd6da82f095e008ee7c1
Andrew Nehalchuk <rtpdfv@yahoo.com>
2022-10-17 16:44:27 +0000

    .

--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
 const express = require("express");
-import { routers } from "./routers.mjs";
+
 
 const app = express();
 const port = 3000;
 
-routers(app);
+app.get("/api/", async (req, res) => {
+    await res.send("123");
+});
+
+app.get("/api/todos", async (req, res) => {
+    await res.json([
+        { name: "Buy some milk" },
+        { name: "Buy some oil" },
+        { name: "Buy some chees" },
+        { name: "Buy some bread" },
+    ]);
+});
+
+// route with dynamic parameter
+app.get("/api/user/", async (req, res) => {
+    await res.json([
+        { id: 0, name: "Jim Doe", phone: "12425323" },
+    ]);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
