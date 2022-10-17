5e0e508f8396a8fd4b7aa5ac1fecdc32ba907fbc
Andrew Nehalchuk <rtpdfv@yahoo.com>
2022-10-17 16:34:20 +0000

    api

--- server/server.js
+++ server/server.js
@@ -1,39 +1,11 @@
 const express = require("express");
+import { routers } from "./routers.mjs";
 
 const app = express();
 const port = 3000;
 
-// const app = new expressive.App();
+routers(app);
 
-// app.use(express.simpleLog());
-// app.use(express.static_("./public"));
-// app.use(express.bodyParser.json());
-
-
-
-
-app.get("/", function (req, res) {
-    res.send("Hello World!");
-});
-
-app.get("/api/todos", async (req, res) => {
-    await res.json([{ name: "Buy some milk" }]);
-});
-// route with dynamic parameter
-app.get("/api/user/{user_id}", async (req, res) => {
-    await res.json([
-        { id: req.params.user_id, name: "Jim Doe", phone: "12425323" },
-    ]);
-});
-
-
-
-
-// const server = await app.listen(port);
-
-
-
-app.listen(port, function () {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
-// console.log("app listening on port " + server.port);
