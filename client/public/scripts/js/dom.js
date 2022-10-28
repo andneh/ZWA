5cfb779750de41ef99d3778f6c8a0ef855de91e6
Andrew Nehalchuk <rtpdfv@yahoo.com>
2022-10-28 11:52:27 +0000

    structure

--- client/public/scripts/js/dom.js
+++ client/public/scripts/js/dom.js
@@ -1,8 +1,11 @@
+const main = document.querySelector('main');
+
 const article = document.querySelector('button.articles');
 const profile = document.querySelector('button.profile');
 const about = document.querySelector('button.about');
 
 export {
+    main,
     article,
     profile,
     about,
--- client/public/scripts/js/index.js
+++ client/public/scripts/js/index.js
@@ -1,37 +1,42 @@
+import * as dom from "./dom.js";
+
 import articles_page from "./components/articles.js";
 import profile_page from "./components/profile.js";
 import about_page from "./components/about.js";
 
 import { articles_data } from "./modules/articles_api.js";
 
-const main = document.querySelector('main');
-import * as nav from "./components/nav.js";
 
 const articles = articles_page(articles_data);
 const profile = profile_page(articles_data);
 const about = about_page(articles_data);
 
+
+
+
 const show_articles = () => {
-    if (main != null) {
-        main.innerHTML = "";
-        main.appendChild(articles);
+    if (dom.main != null) {
+        dom.main.innerHTML = "";
+        dom.main.appendChild(articles);
     }
 }
 const show_profile = () => {
-    if (main != null) {
-        main.innerHTML = "";
-        main.appendChild(profile);
+    if (dom.main != null) {
+        dom.main.innerHTML = "";
+        dom.main.appendChild(profile);
     }
 }
 const show_about = () => {
-    if (main != null) {
-        main.innerHTML = "";
-        main.appendChild(about);
+    if (dom.main != null) {
+        dom.main.innerHTML = "";
+        dom.main.appendChild(about);
     }
 }
 
-nav.article.onclick = show_articles;
-nav.profile.onclick = show_profile;
-nav.about.onclick = show_about;
+dom.article.onclick = show_articles;
+
+dom.profile.onclick = show_profile;
+
+dom.about.onclick = show_about;
 
 show_articles();
