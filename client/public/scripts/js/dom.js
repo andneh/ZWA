35888877503b476953a16c2eadfd1245a222c948
Andrew Nehalchuk <rtpdfv@yahoo.com>
2022-11-10 13:04:46 +0000

    .

--- client/public/scripts/js/dom.js
+++ client/public/scripts/js/dom.js
@@ -1,22 +1,6 @@
-import page from "./components/page";
+nav = {
 
-
-const main = {
-    articles: new page('main>div.articles'),
-    profile: new page('main>div.articles'),
-    about: new page('main>div.articles'),
 }
-
-const nav = {
-    article: document.querySelector('button.articles'),
-    profile: document.querySelector('button.profile'),
-    about: document.querySelector('button.about'),
+main = {
+    
 }
-
-export {
-    main,
-    nav,
-};
-
-
-const a = document.querySelectorAll("main div");
--- client/public/scripts/js/index.js
+++ client/public/scripts/js/index.js
@@ -1,27 +1,41 @@
-// import * as dom from "./dom.js";
 class page {
-    constructor(selector) {
-        this.element = document.querySelector(selector);
-        var self = this;
-        this.show = () => {
-            console.log(self.element);
-            document.querySelectorAll("main>div").forEach(e => e.style.display = "none");
-            self.element.style.display = "block";
+    constructor(selector, manager = undefined) {
+        this.div = document.querySelector(selector);
+        if (manager != undefined) {
+            this.manager = manager;
+            this.load = this.manager.load()
         }
-    };
+    }
+
+    load() {
+
+    }
+
+    show() {
+        document.querySelectorAll("main>div").forEach(e => e.style.display = "none");
+        this.div.style.display = "block";
+    }
+
 }
+
+class nav {
+    constructor(selector, page) {
+        this.button = document.querySelector(selector);
+        this.button.addEventListener('click', () => { page.show(); });
+    }
+}
+
 
 
 const main = {
-    articles: new page('main div.articles'),
-    profile: new page('main div.articles'),
-    about: new page('main div.articles'),
-}
-const nav = {
-    article: new button('button.articles'),
-    profile: new button('button.profile'),
-    about: new button('button.about'),
-}
+    articles: new page('main>div.articles'),
+    profile: new page('main>div.profile'),
+    about: new page('main>div.about'),
+};
+const navig = {
+    articles: new nav('nav>button.articles', main.articles),
+    profile: new nav('nav>button.profile', main.profile),
+    about: new nav('nav>button.about', main.about),
+};
 
-
-dom.main.articles.show();
+main.profile.show();
