42493ebd7c23562b94c5aba7951c74d2cab08b7f
Andrew Nehalchuk <rtpdfv@yahoo.com>
2023-01-15 19:52:18 +0100

    pagination

--- components/_articlesWrapper.php
+++ components/_articlesWrapper.php
@@ -2,48 +2,92 @@
 
 
     <form action="" method="post" class="filter">
-        <p>
+        <fieldset>
+            <legend>
+                Filtr
+            </legend>
+            <p>
+                <label for="cars">Třídit podle:</label>
+                <select name="filter">
+                    <option value="date">Datumu</option>
+                    <option value="title">Názvu</option>
+                    <option value="author">Autora</option>
+                </select>
+            </p>
+            <p>
+                <button type="submit">
+                    Zobrazit
+                </button>
+            </p>
+            <p>
 
-            <label for="cars">Třídit podle:</label>
-
-            <select name="filter">
-                <option value="date">Datumu</option>
-                <option value="title">Názvu</option>
-                <option value="author">Autora</option>
-            </select>
-        </p>
-        <p>
-            <input type="submit" value="filter" name="search">
-        </p>
+            </p>
+        </fieldset>
     </form>
 
 
     <?php
-    require("lib/_sortArticles.php");
-    // $articles;
-    // $btnAct;
-    // $btnNm;
+    // require("lib/_sortArticles.php");
+    // // $articles;
+    // // $btnAct;
+    // // $btnNm;
     
     if (isset($articles)) {
-        echo "filer";
-        if (isset($_POST['filter'])) {
-            echo $_POST['filter'];
-            switch ($_POST['filter']) {
-                case "date":
-                    echo "date";
-                    usort($articles, "sortArticlesByDate");
-                    break;
-                case "title":
-                    echo "title";
-                    usort($articles, "sortArticlesByTitle");
-                    break;
-                case "author":
-                    echo "author";
-                    usort($articles, "sortArticlesByUid");
-                    break;
-            }
+
+        //     if (isset($_POST['filter'])) {
+    
+        //         switch ($_POST['filter']) {
+        //             case "date":
+    
+        //                 usort($articles, "sortArticlesByDate");
+        //                 break;
+        //             case "title":
+    
+        //                 usort($articles, "sortArticlesByTitle");
+        //                 break;
+        //             case "author":
+    
+        //                 usort($articles, "sortArticlesByUid");
+        //                 break;
+        //         }
+        //     }
+    
+        if (isset($_POST['page'])) {
+            $page = $_POST['page'];
+        } else {
+            $page = 0;
         }
-        foreach ($articles as $article) {
+
+
+
+        $articles_keys = array_keys($articles);
+
+
+
+        $numOfArticles = count($articles);
+        switch ($true) {
+            case $numOfArticles < 10:
+                $page_keys = $articles_keys;
+                break;
+
+            case $numOfArticles >= $page + 10:
+                $page_keys = array_slice($articles_keys, $page, 10);
+                break;
+
+            case $numOfArticles < $page + 10:
+                $page_keys = array_slice($articles_keys, $page);
+                break;
+        }
+
+        $articles_page = array();
+
+        foreach ($page_keys as $key) {
+            echo $key;
+            array_push($articles_page, $articles[$key]);
+        }
+
+
+        foreach ($articles_page as $article) {
             $user = getUserByUid($article["uid"]);
             $autor = $user["fname"] . " " . $user["lname"];
             $title = $article["title"];
@@ -80,3 +124,24 @@
     // TODO PAGINATION
     ?>
 </div>
+
+<form action="" method="post">
+    <fieldset>
+        <legend>
+            Pages
+        </legend>
+        <p>
+            <?php
+            $pages = count($articles) / 10;
+            for ($i = 1; $i != $pages; $i++) {
+                echo "<p>";
+                echo "<input type=\"submit\" name=\"page\" value={$i} >";
+                echo "</p>";
+            }
+            ?>
+        </p>
+        <?php
+        echo "<p>{$page}/{$pages}</p>";
+        ?>
+    </fieldset>
+</form>
